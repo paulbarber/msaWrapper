83890445343267f4a7dbcefba9458726de2385e3
Paul Barber <paul.barber@kcl.ac.uk>
2023-09-22 15:36:26 +0100

    Added ROC AUC measure.

--- R/outcomeCorrelations.R
+++ R/outcomeCorrelations.R
@@ -11,43 +11,64 @@
 #' @param groups To restrict covars to those in certain groups.
 #' @export
 #'
-generateOutcomeCorrelations <- function(msa, method = c("kendall", "pearson", "spearman")){
-
+generateOutcomeCorrelations <- function (msa, method = c("kendall", "pearson", "spearman", "AUC")) 
+{
   method <- match.arg(method)
-
-  if(class(msa) == "msaWrapperTte"){
-    outcome <- ifelse(msa$outcome$event, msa$outcome$time, NA)
-  } else {
+  if (class(msa) == "msaWrapperTte") {
+    outcome <- ifelse(msa$outcome$event, msa$outcome$time, 
+                      NA)
+  }
+  else {
     outcome <- as.numeric(msa$outcome)
   }
-
+  
   all_data <- msa$data
+  
   covars <- vector()
+  
   correlation <- vector()
   logpval <- vector()
-
-  for(i in 1:dim(all_data)[2]){
-
-    covar <- all_data[[colnames(all_data)[i]]]
-
-    t = cor.test(covar, outcome, method = method)
-
-    covars <- c(covars, colnames(all_data)[i])
-    correlation <- c(correlation, t$estimate)
-    logpval <- c(logpval, t$p.value)
-    # take log below
+  
+  
+  if(method == "AUC"){
+    
+    if (length(unique(outcome)) > 2)  # not binary outcome
+      stop()
+    
+    for (i in 1:dim(all_data)[2]) {
+      covar <- all_data[[colnames(all_data)[i]]]
+      
+      p <- suppressMessages(pROC::roc(outcome, covar))
+      
+      ci <- pROC::ci.auc(p)
+      
+      # Calc p value from ci (https://www.bmj.com/content/343/bmj.d2304)
+      se = (ci[3]-ci[1])/(2*1.96)   # calc std err
+      z = (ci[2]-0.5)/se            # use difference of estimate to NULL
+      p.value = exp(-0.717*z - 0.416*z^2)
+      
+      covars <- c(covars, colnames(all_data)[i])
+      correlation <- c(correlation, ci[2])
+      logpval <- c(logpval, p.value)
+    }
+    
+  } else {
+    for (i in 1:dim(all_data)[2]) {
+      covar <- all_data[[colnames(all_data)[i]]]
+      t = cor.test(covar, outcome, method = method)
+      covars <- c(covars, colnames(all_data)[i])
+      correlation <- c(correlation, t$estimate)
+      logpval <- c(logpval, t$p.value)
+    }
   }
-
-  #logpval <- log10(p.adjust(logpval, method = "fdr"))
+  
+  
   logpval <- log10(logpval)
-
   cor_data <- data.frame(covars, correlation, logpval)
-
   msa$outcomeCorrelations <- cor_data
-
+  msa$outcomeCorrelationsMethod <- method
   return(msa)
 }
-
 
 #' plotOutcomeCorrelations
 #'
@@ -63,20 +84,21 @@ generateOutcomeCorrelations <- function(msa, method = c("kendall", "pearson", "s
 #' @export
 #'
 plotOutcomeCorrelations <- function(msa, labelAlpha=0.1, hlineAlpha=0.05, textSize = 3, groups = vector()){
-
+  
   if(dim(msa$outcomeCorrelations)[1] < 1) stop()
-
+  
   if(length(groups)>0){
     cor_data <- msa$outcomeCorrelations[msa$group %in% groups,]
   } else {
     cor_data <- msa$outcomeCorrelations
   }
-
+  
   e <- ifelse(cor_data$logpval < log10(labelAlpha), 1, 0)  #Display Signature anems if p values is lower than Significance
-
+  
   ggplot(cor_data, aes(x = correlation, y = -logpval)) +
     geom_point() +
     geom_hline(aes(yintercept = -log10(hlineAlpha)), colour="light grey", linetype="dashed") +
-    geom_text(data = cor_data[e==1,], aes(label = covars), size = textSize)
-
+    geom_text(data = cor_data[e==1,], aes(label = covars), size = textSize) +
+    labs(x = msa$outcomeCorrelationsMethod)
+  
 }
